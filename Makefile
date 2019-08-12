bacd6189dde123f4642037293548a296dd85d7da
szk2s <suzukisjss@gmail.com>
2019-08-12 21:16:41 +0900

    WIP: Connect to DB

--- Makefile
+++ Makefile
@@ -5,6 +5,8 @@ docker-run:
 	--mount type=bind,source="$(PWD)",dst=/app \
 	-p 5000:5000 \
 	flaskappimage
+docker-run-db:
+	docker run --name test-db -e MYSQL_ROOT_PASSWORD=tmppw -d -p 3306:3306 mysql
 docker-logs:
 	docker logs -f flaskapp
 lint:
@@ -15,3 +17,5 @@ type-check:
 	docker exec flaskapp mypy ./**/*.py
 test:
 	docker exec flaskapp pytest
+mysql:
+	mysql -h 127.0.0.1 --port 3306  -uroot -p
--- src/app.py
+++ src/app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-print('Successfully imported modules')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:tmppw@localhost/testdb'
+app.config['SECRET_KEY'] = 'tmppw'
+db = SQLAlchemy(app)
+db.create_all()
 
 
 # %%
@@ -9,3 +13,29 @@ print('Successfully imported modules')
 def hello_world():
     print('Returning Hello World')
     return '<h1>Hello, World!</h1>'
+
+
+@app.route('/about')
+def introduce():
+    print('Returning introduction')
+    return '<p>My Name is Satoshi Suzuki</p>'
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+
+    def __repr__(self):
+        return self.username
+
+
+@app.route('/user', methods=['GET', 'POST'])
+def user():
+    if request.method == 'POST':
+        db.session.add(User(username="Flask", email="example@example.com"))
+        db.session.commit()
+        return 'added'
+    if request.method == 'GET':
+        users = User.query.all()
+        return users
