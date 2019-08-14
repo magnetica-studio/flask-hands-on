8e3d5f783d9cfa6809936192f6f22088a768ebdc
szk2s <suzukisjss@gmail.com>
2019-08-14 11:27:45 +0900

    Set up DB

--- web/src/app.py
+++ web/src/app.py
@@ -2,9 +2,20 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:tmppw@localhost/testdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:tmppw@db/app'
 app.config['SECRET_KEY'] = 'tmppw'
 db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    email = db.Column(db.String(64), unique=True, nullable=False)
+
+    def __repr__(self):
+        return self.username
+
+
 db.create_all()
 
 
@@ -21,21 +32,13 @@ def introduce():
     return '<p>My Name is Satoshi Suzuki</p>'
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
-    email = db.Column(db.String, unique=True, nullable=False)
-
-    def __repr__(self):
-        return self.username
-
-
 @app.route('/user', methods=['GET', 'POST'])
 def user():
     if request.method == 'POST':
-        db.session.add(User(username="Flask", email="example@example.com"))
+        db.session.add(User(username="Satoshi", email="satoshi@magnetica-studio.com"))
         db.session.commit()
         return 'added'
     if request.method == 'GET':
         users = User.query.all()
-        return users
+        print('Users: ', users)
+        return 'got'
